fe399107193ac4d884ff9189c974c53b4d77872f
kimberly blanco <michel_kimberly@hotmail.com>
2017-07-20 11:52:37 -0500

    se crearon formularios

--- NURCON/WindowsApp/Formulario Nurcon/Pacientes.cs
+++ NURCON/WindowsApp/Formulario Nurcon/Pacientes.cs
@@ -16,5 +16,15 @@ namespace WindowsApp.Formulario_Nurcon
         {
             InitializeComponent();
         }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
     }
 }
