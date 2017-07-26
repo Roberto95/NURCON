56de3ccf9dbbae761d2d7b2d61eb8cfc5dcbd1c5
Roberto Reyes <robertoc-95@hotmail.com>
2017-07-26 10:40:28 -0500

    Agregar icono a la ventana de login

--- NURCON/WindowsApp/Formulario Nurcon/FrmUsuarios.Designer.cs
+++ NURCON/WindowsApp/Formulario Nurcon/FrmUsuarios.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmUsuarios));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
@@ -111,6 +112,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "FrmUsuarios";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
