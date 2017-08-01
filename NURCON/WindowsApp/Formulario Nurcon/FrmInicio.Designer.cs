5ebdd05dd7e2a42d194c08a86646ed00ddb9bded
kimberly blanco <michel_kimberly@hotmail.com>
2017-08-01 13:18:31 -0500

    modificar

--- NURCON/WindowsApp/Formulario Nurcon/FrmInicio.Designer.cs
+++ NURCON/WindowsApp/Formulario Nurcon/FrmInicio.Designer.cs
@@ -98,7 +98,7 @@
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(181, 90);
             this.button6.TabIndex = 5;
-            this.button6.Text = "button6";
+            this.button6.Text = "Eliminar";
             this.button6.UseVisualStyleBackColor = true;
             // 
             // button7
